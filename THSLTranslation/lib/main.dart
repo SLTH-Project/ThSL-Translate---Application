bca5da1b62473085ef3ccb02b74688fdfdc8c927
Piyada Thongtem <45333950+walkergameboy@users.noreply.github.com>
2021-10-26 02:58:50 +0700

    Update main.dart

--- THSLTranslation/lib/main.dart
+++ THSLTranslation/lib/main.dart
@@ -16,18 +16,3 @@ Future<void> main() async {
       theme: ThemeData(fontFamily: 'Anakotmai'),
       home: GetStarted(camera: firstCamera)));
 }
-
-/*class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'THSL APP',
-        theme: ThemeData(fontFamily: 'Anakotmai'
-            //primaryColor: const Color(0x)
-            ),
-        //home: const MyHomePage(title: 'THSL Translation'),
-        home: GetStarted());
-  }
-}*/
